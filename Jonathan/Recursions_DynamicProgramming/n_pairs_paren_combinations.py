85fcd23c437856c4cdeb87da5cf6b711e1f0efda
hyuglim <hyuglim@gmail.com>
2016-06-26 01:50:24 -0700

    6/26/2015: n pairs of valid parentheses

---
+++ Jonathan/Recursions_DynamicProgramming/n_pairs_paren_combinations.py
@@ -0,0 +1,47 @@
+# given number of pairs of parentheses
+# return all valid pairs 
+
+def get_valid_parentheses(num):
+    paren_combinations = []
+    get_all_permutations("", "()" * num, paren_combinations)
+    return [
+               paren_pair for paren_pair in paren_combinations
+               if is_valid_pairs(paren_pair)
+           ]
+
+
+
+def get_all_permutations(prefix, suffix, permutations):
+    if not suffix:
+        permutations.append(prefix)
+
+    for i in xrange(len(suffix)):
+        get_all_permutations(
+                    prefix + suffix[i],
+                    suffix[:i] + suffix[i+1:],
+                    permutations
+                )
+
+
+def is_valid_pairs(parens):
+    open_paren_stack = []
+    for paren in parens:
+        if paren == '(':
+            open_paren_stack.append(paren)
+        elif paren == ')':
+            if not open_paren_stack:
+                return False
+            else:
+                open_paren_stack.pop()
+        else:
+            raise Exception("invalid input: need paren")
+    return True
+
+
+assert(is_valid_pairs([paren for paren in "()()()()()"]))
+assert(not is_valid_pairs([paren for paren in ")()()()()"]))
+assert(is_valid_pairs([paren for paren in "((()(())))"]))
+assert(not is_valid_pairs([paren for paren in "()(()()))())"]))
+    
+
+assert(all([pair in ['((()))','(()())','(())()','()(())','()()()'] for pair in get_valid_parentheses(3)]))
